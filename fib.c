b116d4f01a6e7aefcad1e5e2f13c45a9c1c97da2
Liuguanshun <lgs156412@gmail.com>
2022-01-19 19:10:11 +0800

    refactor: use dynamic programming

--- fib.c
+++ fib.c
@@ -1,21 +1,25 @@
 #include "stdio.h"
 
 int fib(int n) {
-  if (n == 0) {
-    return 0;
-  } else if (n == 1) {
+  if (n == 1 || n == 2) {
     return 1;
-  } else {
-    return fib(n - 1) + fib(n - 2);
+  } else if (n == 0) {
+    return 0;
   }
+  int pre = 1, cur = 1;
+  for (int i = 3; i <= n; i++) {
+    cur += pre;
+    pre = cur - pre;
+  }
+  return cur;
 }
 
 int main() {
-  int n = 0;
-  // for (int i = 0; i <= 30; i++) { // test 0 ~ 30
-  //   printf("fib %d: %d\n", i, fib(i));
-  // }
-  scanf("%d", &n);
-  printf("%d\n", fib(n));
+  // int n = 0;
+  for (int i = 0; i <= 30; i++) { // test 0 ~ 30
+    printf("fib %d: %d\n", i, fib(i));
+  }
+  // scanf("%d", &n);
+  // printf("%d\n", fib(n));
   return 0;
 }
