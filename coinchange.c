08be778e1a21d11708abfcadb04d90d9c4c28729
Liuguanshun <lgs156412@gmail.com>
2022-01-20 08:06:36 +0800

    refactor: use dp to solve this

--- coinchange.c
+++ coinchange.c
@@ -1,37 +1,43 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-// int min(int *dp, int size) {
-//   int min_value = 0x7fffffff;
-//   for (int i = 0; i < size; i++) {
-//     if (dp[i] < min_value)
-//       min_value = dp[i];
+// int coinChange(int *coins, int coinsSize, int amount) {
+//   if (amount == 0) {
+//     return 0;
+//   } else if (amount < 0) {
+//     return -1;
 //   }
-//   if (min_value == 0x7fffffff)
-//     return min_value - 1;
-//   return min_value;
+//
+//   int min_value = 0x7fffffff;
+//   int val;
+//   // int *dp = malloc(coinsSize * sizeof(int));
+//   for (int i = 0; i < coinsSize; i++) {
+//     val = coinChange(coins, coinsSize, amount - coins[i]);
+//     if (val == -1) {
+//       continue;
+//     }
+//     min_value = min_value < (val + 1) ? min_value : (val + 1);
+//   }
+//   if (min_value != 0x7fffffff)
+//     return min_value;
+//   return -1;
 // }
 
 int coinChange(int *coins, int coinsSize, int amount) {
-  if (amount == 0) {
-    return 0;
-  } else if (amount < 0) {
-    return -1;
+  int *dp = malloc((amount + 1) * sizeof(int));
+  for (int i = 0; i < amount + 1; i++) {
+    dp[i] = amount + 1;
   }
 
-  int min_value = 0x7fffffff;
-  int val;
-  // int *dp = malloc(coinsSize * sizeof(int));
-  for (int i = 0; i < coinsSize; i++) {
-    val = coinChange(coins, coinsSize, amount - coins[i]);
-    if (val == -1) {
-      continue;
+  dp[0] = 0;
+  for (int i = 0; i < amount + 1; i++) {
+    for (int j = 0; j < coinsSize; j++) {
+      if (i - coins[j] < 0)
+        continue;
+      dp[i] = (dp[i] < (dp[i - coins[j]] + 1)) ? dp[i] : (dp[i - coins[j]] + 1);
     }
-    min_value = min_value < (val + 1) ? min_value : (val + 1);
   }
-  if (min_value != 0x7fffffff)
-    return min_value;
-  return -1;
+  return dp[amount] != amount + 1 ? dp[amount] : -1;
 }
 
 int main() {
