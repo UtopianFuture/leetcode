bb198976b76721ffc219793fa4804328d4e19668
Liuguanshun <lgs156412@gmail.com>
2022-03-20 21:37:34 +0800

    refactor: replaceWords

--- README.md
+++ README.md
@@ -1325,5 +1325,7 @@ this is leetcode exercise.
   若搜索到了前缀的末尾，说明该前缀存在，若前缀末尾对应的节点 isEnd 为真，  
   说明存在该字符串。  
 
+144. [replaceWords](https://leetcode-cn.com/problems/replace-words/)  
+  
 ### reference
 [1] https://github.com/labuladong/fucking-algorithm/blob/master/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%92%E7%B3%BB%E5%88%97/%E9%AB%98%E6%A5%BC%E6%89%94%E9%B8%A1%E8%9B%8B%E8%BF%9B%E9%98%B6.md
---
+++ replaceWords.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+// #include "../dbg.hpp"
+using namespace std;
+#define REOPEN_READ freopen("/home/guanshun/GDB/cpp/input.txt", "r", stdin);
+
+class Trie {
+private:
+  vector<Trie *> children;
+  bool isEnd;
+
+public:
+  Trie() : children(26), isEnd(false){};
+  string search(string word) {
+    Trie *node = this;
+    string res;
+    for (int i = 0; i < (int)word.length(); i++) {
+      word[i] -= 'a';
+      if (node->children[word[i]] == NULL) {
+        return "";
+      }
+      res.push_back(word[i] + 'a');
+      node = node->children[word[i]];
+      // this is the last node
+      if (node->isEnd) {
+        break;
+      }
+    }
+    return res;
+  }
+
+  void insert(string word) {
+    Trie *node = this;
+    for (int i = 0; i < (int)word.length(); i++) {
+      word[i] -= 'a';
+      if (node->children[word[i]] == NULL) {
+        node->children[word[i]] = new Trie;
+      }
+      node = node->children[word[i]];
+    }
+    node->isEnd = true;
+  }
+};
+
+class Solution {
+public:
+  string replaceWords(vector<string> &dictionary, string sentence) {
+    Trie *obj = new Trie;
+
+    // build trie
+    for (int i = 0; i < (int)dictionary.size(); i++) {
+      obj->insert(dictionary[i]);
+    }
+
+    string tmp, tmp1;
+    string res = "";
+    // add a " " to the end of the sentence
+    sentence += " ";
+    for (char ch : sentence) {
+      if (ch != ' ') {
+        tmp.push_back(ch);
+      } else {
+        tmp1 = obj->search(tmp);
+        if (tmp1 != "") {
+          res += tmp1;
+          res += " ";
+        } else {
+          res += tmp;
+          res += " ";
+        }
+        tmp.clear();
+      }
+    }
+
+    // erase the last " "
+    res.erase(res.end() - 1, res.end());
+    return res;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  REOPEN_READ;
+  int n;
+  cin >> n;
+  vector<string> dictionary;
+  string tmp;
+  for (int i = 0; i < n; i++) {
+    cin >> tmp;
+    dictionary.push_back(tmp);
+  }
+
+  string sentence = "the cattle was rattled by the battery";
+  Solution *s = new Solution;
+  cout << s->replaceWords(dictionary, sentence) << "\n";
+
+  return 0;
+}
