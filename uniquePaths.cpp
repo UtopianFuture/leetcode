1af78f1fc86a829d5245141b8d2ca1b2640c113f
Liuguanshun <lgs156412@gmail.com>
2022-06-04 12:58:40 +0800

    refactor: uniquePaths

---
+++ uniquePaths.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+// #include "../dbg.hpp"
+using namespace std;
+#define REOPEN_READ freopen("/home/guanshun/GDB/cpp/input.txt", "r", stdin);
+
+class Solution {
+public:
+  int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 1));
+    for (int i = 1; i < m; i++) {
+      for (int j = 1; j < n; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+    return dp[m - 1][n - 1];
+  }
+};
+
+int main(int argc, char *argv[]) {
+  REOPEN_READ;
+  int m, n;
+  cin >> m >> n;
+
+  Solution *s = new Solution;
+  cout << s->uniquePaths(m, n) << endl;
+
+  return 0;
+}
