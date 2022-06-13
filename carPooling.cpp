696376e7b5e375602ef7045048a493dac6969b66
Liuguanshun <lgs156412@gmail.com>
2022-06-13 17:26:42 +0800

    refactor: carPooling

--- carPooling.cpp
+++ carPooling.cpp
@@ -32,6 +32,34 @@ public:
   }
 };
 
+class Solution1 {
+public:
+  bool carPooling(vector<vector<int>> &trips, int capacity) {
+    int n = (int)trips.size();
+    vector<int> res(1001, 0);
+
+    int m = INT_MIN;
+    for (int i = 0; i < n; i++) {
+      res[trips[i][1]] += trips[i][0];
+      if (trips[i][1] != 1000) {
+        res[trips[i][2]] -= trips[i][0];
+      }
+      m = max(m, trips[i][2]);
+    }
+
+    if (res[0] > capacity) {
+      return false;
+    }
+    for (int i = 1; i < m; i++) {
+      res[i] += res[i - 1];
+      if (res[i] > capacity) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
 int main(int argc, char *argv[]) {
   REOPEN_READ;
   int size;
